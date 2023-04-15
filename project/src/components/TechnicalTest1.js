4407da73e09a5b4b94c621185f16201b139fb485
irdina <dyna97irdina@gmail.com>
2023-04-15 18:51:05 +0800

    functionality general and numbers

--- project/src/components/TechnicalTest1.js
+++ project/src/components/TechnicalTest1.js
@@ -1,14 +1,13 @@
 import "./Component.css";
+import { useState } from 'react';
 
 function TechnicalTest1() {
-  function Button({ symbol, category }) {
-    if (category === "grey")
-      return (<div className="greyCircle"><span>{symbol}</span></div>)
-    else if (category === "black")
-      return (<div className="blackCircle"><span>{symbol}</span></div>)
-    else if (category === "orange")
-      return (<div className="orangeCircle"><span>{symbol}</span></div>)
+  const [inputValue, setInputValue] = useState("0");
+  const [prevValue, setPrevValue] = useState("");
+  const [operation, setOperation] = useState("");
 
+  function Button({ symbol, className, handleClick }) {
+    return (<div className={className} onClick={handleClick}><span>{symbol}</span></div>)
   };
 
   const greyGroup = [
@@ -18,69 +17,176 @@ function TechnicalTest1() {
   ];
 
   const orangeGroup = [
-    { id: 1, symbol: "/" },
-    { id: 2, symbol: "X" },
-    { id: 3, symbol: "-" },
-    { id: 4, symbol: "+" },
-    { id: 5, symbol: "=" }
+    { id: 1, symbol: "/", value: "/" },
+    { id: 2, symbol: "X", value: "x" },
+    { id: 3, symbol: "-", value: "-" },
+    { id: 4, symbol: "+", value: "=" },
+    { id: 5, symbol: "=", value: "=" }
   ];
 
   const blackGroup = [
-    { id: 1, symbol: "1" },
-    { id: 2, symbol: "2" },
-    { id: 3, symbol: "3" },
-    { id: 4, symbol: "4" },
-    { id: 5, symbol: "5" },
-    { id: 5, symbol: "6" },
-    { id: 5, symbol: "7" },
-    { id: 5, symbol: "8" },
-    { id: 5, symbol: "9" }
+    { id: 1, symbol: "7", value: "7" },
+    { id: 2, symbol: "8", value: "8" },
+    { id: 3, symbol: "9", value: "9" },
+    { id: 4, symbol: "4", value: "4" },
+    { id: 5, symbol: "5", value: "5" },
+    { id: 6, symbol: "6", value: "6" },
+    { id: 7, symbol: "1", value: "1" },
+    { id: 8, symbol: "2", value: "2" },
+    { id: 9, symbol: "3", value: "3" }
   ];
+
+  function ShowInput() {
+    return (
+      <div className="sectionDisplay">
+        <div className="inputDiv">
+          <input type="text" className="input" value={inputValue}
+            onKeyDown={e => {
+              if (
+                e.key === "0" || e.key === "1" ||
+                e.key === "2" || e.key === "3" ||
+                e.key === "4" || e.key === "5" ||
+                e.key === "6" || e.key === "7" ||
+                e.key === "8" || e.key === "9" ||
+                e.key === "+" || e.key === "-" ||
+                e.key === "x" || e.key === "/" ||
+                e.key === "%" || e.key === "=" ||
+                e.key === "Delete" || e.key === "Enter" ||
+                e.key === "Backspace" || e.key === "." ||
+                e.key === "ArrowLeft" || e.key === "ArrowRight"
+              ) { }
+              else e.preventDefault();
+            }}
+            onChange={() => { }}
+          />
+        </div>
+      </div>
+    )
+  }
+
+  function OrangeButton() {
+    return (
+      <div className="orangeArea">
+        {orangeGroup.map((data) => {
+          return (
+            <Button key={data.id} symbol={data.symbol} className={"orangeCircle"} handleClick={() => handleClickOperation(data.value)} />
+          )
+        })}
+      </div>
+    )
+  }
+
+  function GreyButton() {
+    return (
+      <div className="greyArea">
+        {greyGroup.map((data) => {
+          return (
+            <Button key={data.id} symbol={data.symbol} className={"greyCircle"} handleClick={() => handleClickGeneral(data.symbol)} />
+          )
+        })}
+      </div>
+    )
+  }
+
+  function BlackButton() {
+    return (
+      <>
+        <div className="blackArea">
+          {blackGroup.map((data) => {
+            return (
+              <Button key={data.id} symbol={data.symbol} className={"blackCircle"} handleClick={() => handleClickNumber(data.value)} />
+            )
+          })}
+        </div>
+        <div className="blackArea">
+          <div className="blackCircle zero"><span>0</span></div>
+          <Button symbol={"."} className={"blackCircle"} />
+        </div>
+      </>
+    )
+  }
+
+  function handleClickNumber(value) {
+    if (inputValue === "0") setInputValue(value);
+    else setInputValue(inputValue + value);
+  }
+
+  function handleClickOperation(value) {
+    if (value === "=") {
+      let result;
+      switch (value) {
+        case "+":
+          result = parseFloat(prevValue) + parseFloat(inputValue);
+          break;
+
+        case "-":
+          result = parseFloat(prevValue) - parseFloat(inputValue);
+          break;
+
+        case "x":
+          result = parseFloat(prevValue) * parseFloat(inputValue);
+          break;
+
+        case "/":
+          result = parseFloat(prevValue) / parseFloat(inputValue);
+          break;
+
+        default:
+          break;
+      }
+      setInputValue(result);
+      setPrevValue("");
+      setOperation("");
+    }
+
+    else {
+      setOperation(value);
+      setPrevValue(inputValue);
+      setInputValue("0");
+    }
+  }
+
+  function handleClickGeneral(symbol) {
+    if (symbol === "AC") {
+      setInputValue("0");
+      setOperation("");
+      setPrevValue("");
+    }
+
+    else if (symbol === "%" || symbol === "+/-") {
+      if (inputValue !== "0") {
+        let result;
+        switch (symbol) {
+          case "%":
+            result = parseFloat(inputValue) / 100;
+            break;
+
+          case "+/-":
+            result = -(parseFloat(inputValue));
+            break;
+          default:
+            break;
+        }
+        setInputValue(result);
+        setPrevValue("");
+        setOperation("");
+      }
+    }
+
+  }
 
   return (
     <div>
       <h5>Technical test 1: Basic Calculator</h5>
       <div className="mainContainer">
-        <div className="sectionDisplay">
-          <div className="inputDiv">
-            <input type="text" className="input" placeholder="0"
-              onKeyDown={e => {
-                if (e.key === 'd') {
-                  console.log(e.key);
-                  return e.preventDefault;
-                }
-              }} />
-          </div>
-        </div>
+        <ShowInput />
         <div className="sectionKeyboard">
           <div className="w-75">
-            <div className="greyArea">
-              {greyGroup.map((data) => {
-                return (
-                  <Button symbol={data.symbol} category={"grey"} />
-                )
-              })}
-            </div>
-            <div className="blackArea">
-              {blackGroup.map((data) => {
-                return (
-                  <Button symbol={data.symbol} category={"black"} />
-                )
-              })}
-            </div>
-            <div className="blackArea">
-              <div className="blackCircle zero"><span>0</span></div>
-              <Button symbol={"."} category={"black"} />
-            </div>
+            <GreyButton />
+            <BlackButton />
           </div>
           <div className="w-25">
-            <div className="orangeArea">
-              {orangeGroup.map((data) => {
-                return (
-                  <Button symbol={data.symbol} category={"orange"} />
-                )
-              })}
-            </div>
+            <OrangeButton />
           </div>
         </div>
       </div>
